0b0752eefb4c53754a9844b2c20afac6fac780e9
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-02-21 22:59:51 +0530

    Updated Version

--- sell.js
+++ sell.js
@@ -1,13 +1,12 @@
 let  price = document.getElementById('sell-price');
-let original_price = document.getElementById('ori-price');
+let original_price = document.getElementById('ori-price');//CP
 let minimum_discount = document.getElementById('min-dis');
+let cp = null;
 price.addEventListener('input',()=>{
-   let proprice = (price.value)*(85/100);
-   original_price.innerHTML = price.value-proprice;
+   cp = (100/(100+85))*price.value;
+   original_price.innerHTML = Math.round(cp);
 });
 price.addEventListener('input',()=>{
-    let proprice = (price.value)*(85/100);
-   proprice = price.value-proprice;
-   proprice = proprice+(proprice)*(60/100);
-   minimum_discount.innerHTML = proprice;
+   let min = ((100+60)/100)*cp;
+   minimum_discount.innerHTML = Math.round(min);
 })
