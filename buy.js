6096e519cdbdb315647547b34eba995fa819a608
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-02-21 23:32:06 +0530

    Buy Feature added

---
+++ buy.js
@@ -0,0 +1,7 @@
+let cp = document.getElementById('newbuy');
+let sp = document.getElementById('pricetext');
+
+cp.addEventListener('input',()=>{
+     let price = ((100+85)/100)*cp.value;
+     sp.innerHTML = Math.round(price);
+})
